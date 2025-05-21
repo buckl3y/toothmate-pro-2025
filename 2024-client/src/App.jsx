15e5e501d929778b8123f0c267e935c3c6eee972
buckl3y <cbuckley021@icloud.com>
2025-05-22 10:06:07 +1200

    refactor: refactored App.jsx
    Introduced comments and
    made routes more maintainable with RouterProvider from 'react-router-dom'

--- 2024-client/src/App.jsx
+++ 2024-client/src/App.jsx
@@ -1,20 +1,28 @@
 
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './Components/Dashboard/Dashboard';
-import DisplayWholeMouth from './Components/Chart/DisplayWholeMouth';
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    // <Route path='/' element={<MainLayout />} />
+    // Update this route with a layout element if we end up using layouts
+    // e.g. consistent NavBar, Footer etc.
+  <Route path='/' >
+    {/* Place ROUTES to PAGES here e.g. 
+        <Route path='/patients' element={<PatientsPage/>}/> something like that...
+        For now, the index is the Dashboard page, and the 'views'
+        are handled in Dashboard (isAdminView etc), this is probably going to have to be refactored, but for now it's ok.
+    */}
+    <Route index element={ <Dashboard />} />
+  </Route>
+  )
+);
 
 function App() {
-
-  return (
-    <Router>
-
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-
-    </Router>
-  )
+  // the RouterProvider manages all routes defined in the router variable
+  // we pass the router variable to RouterProvider here.
+  return <RouterProvider router={router}/>
 }
 
 export default App
