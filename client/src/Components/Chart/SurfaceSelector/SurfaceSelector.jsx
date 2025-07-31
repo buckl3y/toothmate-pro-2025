c0bb06db4dce181bdb44bd81892ee7286ad724db
samh812 <132234555+samh812@users.noreply.github.com>
2025-07-31 13:59:38 +1200

    Surface slect buttons, multiple can be selected and deselected. Stlying need smore work

--- client/src/Components/Chart/SurfaceSelector/SurfaceSelector.jsx
+++ client/src/Components/Chart/SurfaceSelector/SurfaceSelector.jsx
@@ -2,34 +2,81 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 
 export default function SurfaceSelector({onSurfaceSelected}) {
-    const [selectedSurface, setSelectedSurface] = useState("");
+    const [selectedSurfaces, setSelectedSurface] = useState([]);
 
     function handleSurfaceSelected(surface) {
-        setSelectedSurface(surface);
+        let updated;
+
+        if (selectedSurfaces.includes(surface)) {
+            updated = selectedSurfaces.filter(s => s !== surface);
+        } else {
+            updated = [...selectedSurfaces, surface];
+        }
+
+        setSelectedSurface(updated);
         onSurfaceSelected(surface);
     }
+
+    const isSelected = (surface) => selectedSurfaces.includes(surface);
 
     return (
         <>
         <div className="grid grid-cols-3 grid-rows-3 gap-2">
             <div></div> {/*whitespace top left*/}
             <div>
-                <button className="bg-gray-300" id="mesial" onClick={() => handleSurfaceSelected("mesial")}>Mesial</button>
+                <button
+                    id="mesial"
+                    onClick={() => {handleSurfaceSelected("mesial");}}
+                    className={isSelected("mesial") ? "btn-surface-selected" : "btn-surface"}
+                >
+                    M
+                </button>
             </div>
+            
             <div></div> {/*whitespace top right*/}
+
             <div>
-                <button id="buccal" onClick={() => handleSurfaceSelected("buccal")}>Buccal</button>
+                <button
+                    id="buccal"
+                    onClick={() => {handleSurfaceSelected("buccal");}}
+                    className={isSelected("buccal") ? "btn-surface-selected" : "btn-surface"}
+                >
+                    B
+                </button>
             </div>
+
             <div>
-                <button id="occlusal" onClick={() => handleSurfaceSelected("occlusal")}>Occlusal</button>
+                <button
+                    id="occlusal"
+                    onClick={() => {handleSurfaceSelected("occlusal");}}
+                    className={isSelected("occlusal") ? "btn-surface-selected" : "btn-surface"}
+                >
+                    O
+                </button>            
             </div>
+
             <div>
-                <button id="lingual" onClick={() => handleSurfaceSelected("lingual")}>Lingual</button>
+                <button
+                    id="lingual"
+                    onClick={() => {handleSurfaceSelected("lingual");}}
+                    className={isSelected("lingual") ? "btn-surface-selected" : "btn-surface"}
+                >
+                    L
+                </button>            
             </div>
+
             <div></div> {/*whitespace bottom left*/}
+
             <div>
-                <button className="bg-gray-300" id="distal" onClick={() => handleSurfaceSelected("distal")}>Distal</button>
+                <button
+                    id="distal"
+                    onClick={() => {handleSurfaceSelected("distal");}}
+                    className={isSelected("distal") ? "btn-surface-selected" : "btn-surface"}
+                >
+                    D
+                </button>            
             </div>
+
             <div></div> {/*whitespace bottom right*/}
         </div>
         </>
