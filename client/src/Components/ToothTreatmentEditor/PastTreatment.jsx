475c532df4053c715f9f21bbe85ed53cde216a35
Caleb Buckley <cbuckley021@icloud.com>
2025-08-01 15:39:37 +1200

    fix: mini commit
    just added .name to see patient name in pasttreatment.jsx

--- client/src/Components/ToothTreatmentEditor/PastTreatment.jsx
+++ client/src/Components/ToothTreatmentEditor/PastTreatment.jsx
@@ -3,8 +3,8 @@ const PastTreatment = ({ selectedTooth, selectedPatient, selectedDate }) => {
         <div className="flex flex-col items-center">
             <span className="text-lg font-semibold">Past Treatment</span>
             <span className="text-sm mt-2">Selected Tooth: {selectedTooth}</span>
-        {/* <span className="text-sm mt-2">Selected Patient: {selectedPatient}</span>
-            <span className="text-sm mt-2">Selected Date: {selectedDate}</span> */}
+            <span className="text-sm mt-2">Selected Patient: {selectedPatient.name}</span>
+            {/* <span className="text-sm mt-2">Selected Date: {selectedDate}</span> */}
 
         </div>
     );
